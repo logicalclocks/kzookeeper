867f18752195cf7c6522ed3750451ac3eefee786
jimdowling <jdowling@kth.se>
2015-08-07 10:47:14 +0200

    Code generated by Karamel.

---
+++ attributes/default.rb
@@ -0,0 +1,8 @@
+default[:kzookeeper][:url] = ""
+default[:kzookeeper][:version] = "0.1"
+default[:kzookeeper][:user] = "zookeeper"
+default[:kzookeeper][:group] = "zookeeper"
+
+default[:kzookeeper][:version_dir] = "/usr/local/kzookeeper-#{node[:kzookeeper][:version]}"
+default[:kzookeeper][:home_dir] = "/usr/local/kzookeeper"
+
---
+++ metadata.rb
@@ -0,0 +1,33 @@
+name             'kzookeeper'
+maintainer       "zookeeper"
+maintainer_email "jdowling@kth.se"
+license          "Apache v2.0"
+description      'Installs/Configures/Runs kzookeeper'
+version          "0.1"
+
+recipe            "kzookeeper::install", "Experiment setup for kzookeeper"
+
+
+
+
+%w{ ubuntu debian rhel centos }.each do |os|
+  supports os
+end
+
+attribute "kzookeeper/version",
+:description => "Version of kzookeeper",
+:type => 'string',
+:default => "0.1"
+
+
+attribute "kzookeeper/url",
+:description => "Url to download binaries for kzookeeper",
+:type => 'string',
+:default => ""
+
+attribute "kzookeeper/user",
+:description => "Run kzookeeper as this user",
+:type => 'string',
+:default => "kzookeeper"
+
+
