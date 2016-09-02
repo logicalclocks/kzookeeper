a21259907562edf6fd227a29f3cdd7f4c7c473ff
Jim Dowling <jdowling@sics.se>
2016-09-02 15:56:32 +0200

    recursively create install dir

--- recipes/install.rb
+++ recipes/install.rb
@@ -1,1 +1,23 @@
 # Nothing to do here...
+
+user node.kzookeeper.user do
+  action :create
+  supports :manage_home => true
+  home "/home/#{node.kzookeeper.user}"
+  shell "/bin/bash"
+  not_if "getent passwd #{node.kzookeeper.user}"
+end
+
+group node.kzookeeper.group do
+  action :modify
+  members ["#{node.kzookeeper.user}"]
+  append true
+end
+
+directory "#{node.kzookeeper.install_dir}" do
+  owner node.kzookeeper.user
+  group node.kzookeeper.group
+  mode "755"
+  action :create
+  recursive true
+end
