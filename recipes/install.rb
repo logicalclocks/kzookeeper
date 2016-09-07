fb9cf5d17c655c2c22fe064416760d286c39793c
Jim Dowling <jdowling@sics.se>
2016-09-07 09:31:17 +0200

    Needs patch and other devtools for centos

--- recipes/install.rb
+++ recipes/install.rb
@@ -26,7 +26,12 @@ end
 case node.platform_family
 
   when "rhel"
-  package "patch" do
-    action :install
-  done
+
+  bash "install_patch_and_other_devtools" do
+    user "root"
+    code <<-EOF
+    yum groupinstall 'Development Tools' end
+  EOF
+
 end
+
