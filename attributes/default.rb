2d76b3d689bf64885615d77196b7b2c60e92c789
Jim Dowling <jdowling@sics.se>
2017-03-08 15:56:03 +0100

    install/user now a global attr

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,8 +2,8 @@ include_attribute "kagent"
 
 default.kzookeeper.version                   = '3.4.7'
 default.kzookeeper.url                       = ""
-default.kzookeeper.user                      = "zookeeper"
-default.kzookeeper.group                     = "zookeeper"
+default.kzookeeper.user                      = node.install.user.empty? ? "zookeeper" : node.install.user
+default.kzookeeper.group                     = node.install.user.empty? ? "zookeeper" : node.install.user
 
 
 default.kzookeeper.dir                       = node.install.dir.empty? ? "/opt" : node.install.dir
