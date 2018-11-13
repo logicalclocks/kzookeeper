9ce12356f26b1bc9a73b526089e4dc9c5ffcf3f4
Fabio Buso <buso.fabio@gmail.com>
2018-11-13 23:50:55 +0100

    [HOPSWORKS-713] Make zookeeper binary discoverable by setup-chef

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,6 +18,9 @@ default['kzookeeper']['default']['private_ips'] = ['10.0.2.15']
 default['kzookeeper']['checksum']                  = '2e043e04c4da82fbdb38a68e585f3317535b3842c726e0993312948afcc83870'
 default['kzookeeper']['mirror']                    = node['download_url']
 
+# This attribute is here so that it can be parsed by setup-chef. The download url is re-built by the default provider
+default['kzookeeper']['download_url']              = ::File.join(node['download_url'], "zookeeper-#{node['kzookeeper']['version']}", "zookeeper-#{node['kzookeeper']['version']}.tar.gz")
+
 default['kzookeeper']['use_java_cookbook']         = true
 
 default['kzookeeper']['config'] = {
