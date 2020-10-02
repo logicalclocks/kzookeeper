4dfa1a46e8866e00780b44491fd6be45781b5914
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2020-10-02 12:16:48 +0200

    [HOPSWORKS-2046] wait for node to join cluster (#17)

--- recipes/default.rb
+++ recipes/default.rb
@@ -265,3 +265,14 @@ consul_service "Registering ZooKeeper with Consul" do
   })
   action :register
 end
+
+# Wait until node was joined the cluster. The "zkServer.sh status" command exits with "1" if the node is not
+# connected to the cluster, otherwise it exits with "0"
+bash 'check_node_status' do
+  user "root"
+  group "root"
+  timeout 120
+  code <<-EOH
+       until #{node['kzookeeper']['install_dir']}/zookeeper/bin/zkServer.sh status; do sleep 1; done
+  EOH
+end
