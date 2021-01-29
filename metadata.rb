11aded71c3f9b018d59dd380163624dbeb665b78
Theofilos Kakantousis <t.kakantousis@gmail.com>
2021-01-29 13:20:59 +0100

    Release version: 2.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "jdowling@kth.se"
 name             "kzookeeper"
 license          "Apache v2.0"
 description      'Installs/Configures/Runs kzookeeper'
-version          "2.1.0"
+version          "2.2.0"
 
 %w{ ubuntu debian rhel centos }.each do |os|
   supports os
