992cb1bf90bd2bb834598bf81285134ef24c98ec
Fabio Buso <buso.fabio@gmail.com>
2021-11-24 00:22:56 +0100

    [HOPSWORKS-2835] Zookeeper recipe does not respect external_user option
    The user and group are already created in the default recipe. We don't
    need to re-create them in the default provider as well.
    
    Moreover in the default recipe the external_user option is configured
    correctly.

--- recipes/install.rb
+++
@@ -1,9 +0,0 @@
-case node['platform_family']
-when "rhel"
-  bash "install_patch_and_other_devtools" do
-    user "root"
-    code <<-EOF
-      yum groupinstall 'Development Tools' -y
-  EOF
-  end
-end
