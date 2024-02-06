fa1324402a7030de06c066e0eec46216c02b3355
Fabio Buso <fabio@hopsworks.ai>
2024-02-06 18:16:57 +0100

    Prepare development for: 3.8.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "jdowling@kth.se"
 name             "kzookeeper"
 license          "Apache v2.0"
 description      'Installs/Configures/Runs kzookeeper'
-version          "3.7.0"
+version          "3.8.0"
 
 %w{ ubuntu debian rhel centos }.each do |os|
   supports os
